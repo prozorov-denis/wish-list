eb6baa1d876d0b0a6d2bcfe954f4ddd3b3293165
Denis Prozorov <prozorov.denis.work@gmail.com>
2024-02-10 12:58:27 +0300

    add mui

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import type { FC } from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 
-export const App: React.FunctionComponent = () => {
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+export const App: FC = () => {
   return <RouterProvider router={router} />;
 };
